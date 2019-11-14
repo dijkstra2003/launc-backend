54812cb05b893f3ddfbefc8aaa5f6053ee9f7956
Joey de Ruiter <contact@joeyderuiter.me>
2019-11-14 13:23:18 +0100

    Tag non release branches always as latest

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,12 @@
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker build -t launc .
 docker images
-docker tag launc:latest $DOCKER_USERNAME/launc-backend:$1
+
+if [ $1 == "release" ]; then
+    docker tag launc:latest $DOCKER_USERNAME/launc-backend:$1
+else
+    docker tag launc:latest $DOCKER_USERNAME/launc-backend:latest
+    docker tag launc:latest $DOCKER_USERNAME/launc-backend:$1
+fi
+
 docker push $DOCKER_USERNAME/launc-backend
