131f08b9a7c32d98675851a3c636a12b035e2339
Joey de Ruiter <contact@joeyderuiter.me>
2019-11-14 10:22:07 +0100

    Fixed the way tags are named

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker build -t launc .
 docker images
-docker tag launc:$1 $DOCKER_USERNAME/launc-backend
+docker tag launc:latest $DOCKER_USERNAME/launc-backend:$1
 docker push $DOCKER_USERNAME/launc-backend
