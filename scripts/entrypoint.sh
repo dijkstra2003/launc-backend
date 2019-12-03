ffee5eeea7ec454353e3c4ed335a7a32d2d30955
Joey de Ruiter <contact@joeyderuiter.me>
2019-12-03 16:44:33 +0100

    Use env from .bashrc and change into the directory for the right current_dir path

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -6,4 +6,6 @@ dotnet ef database update -c DataContext \
      -p ./src/API.Infrastructure/API.Infrastructure.csproj \
      -s ./src/API.Web/API.Web.csproj
 
-dotnet out/API.Web.dll
+
+cd out/
+dotnet API.Web.dll
