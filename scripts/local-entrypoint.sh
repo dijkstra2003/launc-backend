afc1a6faa0a5e331928f758d1ce9b048007e1c85
Joey de Ruiter <contact@joeyderuiter.me>
2019-12-03 14:50:29 +0100

    Database + migrations now work in local environment

--- scripts/local-entrypoint.sh
+++ scripts/local-entrypoint.sh
@@ -2,14 +2,9 @@
 
 set -e
 
-dotnet build
+dotnet ef database update -c DataContext \
+     -p ./src/API.Infrastructure/API.Infrastructure.csproj \
+     -s ./src/API.Web/API.Web.csproj
 
-#dotnet build
-
-#dotnet ef database update -c DataContext \
-#     -p ./src/API.Infrastructure/API.Infrastructure.csproj \
-#     -s ./src/API.Web/API.Web.csproj
-
-# dotnet run watch --project src/API.Web --urls "http://0.0.0.0:5000;http://[::]:5000"
-# dotnet run watch --project src/API.Web
+ dotnet run watch --project src/API.Web --urls "http://0.0.0.0:5000;http://[::]:5000"
 
