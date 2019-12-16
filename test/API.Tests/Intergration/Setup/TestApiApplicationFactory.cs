e87fa1b1877aa88ed18413875c0bf228d2ff5c93
Joey de Ruiter <contact@joeyderuiter.me>
2019-12-16 13:34:13 +0100

    You can now make authenticated & authorized request with the factory.CreateAuthClient client.

--- test/API.Tests/Intergration/Setup/TestApiApplicationFactory.cs
+++ test/API.Tests/Intergration/Setup/TestApiApplicationFactory.cs
@@ -11,40 +11,33 @@ using System;
 using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.EntityFrameworkCore.Storage;
 using System.Linq;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using System.Net.Http;
 
 namespace API.Tests.Intergration.Setup
 {
-    public class TestApiApplicationFactory<TStartup> : WebApplicationFactory<Startup>
+    public class TestApiApplicationFactory<TStartup> : WebApplicationFactory<Startup> where TStartup: class
     {
         protected override void ConfigureWebHost(IWebHostBuilder builder)
         {
             builder.ConfigureServices(services => {
-
-                var serviceProvider = new ServiceCollection()
-                    .AddEntityFrameworkInMemoryDatabase()
-                    .BuildServiceProvider();
                 
-                // services.AddAutoMapper(typeof(TStartup));
-
-                services.AddMvc(
-                    options => {
-                        options.Filters.Add(new AllowAnonymousFilter());
-                        options.Filters.Add(new FakeUserFilter());
-                    }
-                );
-
                 // Remove the app's DataContext registration.
-                var descriptor = services.SingleOrDefault(d => d.ServiceType == typeof(DbContextOptions<DataContext>));
-
-                if (descriptor != null)
                 {
-                    services.Remove(descriptor);
+                    var descriptor = services.SingleOrDefault(d => d.ServiceType == typeof(DbContextOptions<DataContext>));
+
+                    if (descriptor != null)
+                    {
+                        services.Remove(descriptor);
+                    }
                 }
 
                 // Use an in memory database for testing.
                 services.AddDbContext<DataContext>(options => {
                     options.UseInMemoryDatabase("InMemoryTestingDb");
-                    options.UseInternalServiceProvider(serviceProvider);
                 });
 
                 // Dependency injection (cannot use singletons because of the conflicting scope with the DbContext.)
@@ -68,10 +61,40 @@ namespace API.Tests.Intergration.Setup
                     catch (Exception ex)
                     {
                         logger.LogError(ex, "An error occurred seeding the " +
-                            "database with test messages. Error: {ex.Message}");
+                            "database with test data. Error: {ex.Message}");
                     }
                 }
             });        
+        }
+
+        public HttpClient CreateAuthClient() {
+            var client = this.WithWebHostBuilder(builder => {
+                builder.ConfigureTestServices(services => {
+
+                    services
+                        .AddAuthentication("Test")
+                        .AddScheme<AuthenticationSchemeOptions, TestAuthHandler>("Test", options => {});
+
+                    // Allow both the JWT authentication and our Test authentication method
+                    services.AddAuthorization(options => {
+                        var defaultAuthorizationPolicyBuilder = new AuthorizationPolicyBuilder(
+                            JwtBearerDefaults.AuthenticationScheme,
+                            "Test");
+
+                        defaultAuthorizationPolicyBuilder = defaultAuthorizationPolicyBuilder.RequireAuthenticatedUser();
+
+                        options.DefaultPolicy = defaultAuthorizationPolicyBuilder.Build();
+                    });
+                    
+                    
+                });
+            }).CreateClient(new WebApplicationFactoryClientOptions { 
+                AllowAutoRedirect = false 
+            });
+
+            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Test");
+
+            return client;
         }
     }
 }
