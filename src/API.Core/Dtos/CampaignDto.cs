a8abe8cae54506a755795a5736503ac3ea3f9075
Joey de Ruiter <contact@joeyderuiter.me>
2020-01-08 19:28:55 +0100

    Show campaign id in the campaign dto

--- src/API.Core/Dtos/CampaignDto.cs
+++ src/API.Core/Dtos/CampaignDto.cs
@@ -2,6 +2,7 @@ namespace API.Core.Dtos
 {
     public class CampaignDto
     {
+        public int Id { get; set; }
         public string CampaignName { get; set; }
         public string CampaignDescription { get; set; }
         public GoalDto Goal { get; set; }
